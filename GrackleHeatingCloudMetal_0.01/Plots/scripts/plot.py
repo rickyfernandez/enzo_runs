7ea68afd4a6a2c217630567e7e431bbdec71c038
Ricardo Fernandez <rafernandezjr@gmail.com>
2015-09-08 08:07:54 -0500

    added grackle heat run Z=0.01*Z_sun

---
+++ GrackleHeatingCloudMetal_0.01/Plots/scripts/plot.py
@@ -0,0 +1,17 @@
+import yt
+
+plot_range = range(5) 
+
+file_names = []
+for i in plot_range:
+    file_names.append("../../DD" + `i`.zfill(4) + "/" + "cloud_collision_" + `i`.zfill(4))
+
+for file in file_names:
+    ds = yt.load(file)
+    #p = yt.SlicePlot(ds, "z", "velocity_magnitude")
+    p = yt.SlicePlot(ds, "z", "temperature")
+    #p.set_zlim("number_density", 5.0E-2, 1.0E3)
+    #p.set_zlim("pressure", 3.0E-13, 4.0E-10)
+    p.set_zlim("temperature", 1.0E1, 2.0E6)
+    #p.annotate_grids()
+    p.save()
---
+++ GrackleHeatingCloudMetal_0.01/Plots/scripts/velocity_disperssion.py
@@ -0,0 +1,45 @@
+import yt
+import numpy as np
+import matplotlib.pyplot as plt
+
+plot_range = range(5) 
+
+num_plots = len(plot_range)
+colormap = plt.cm.gist_ncar
+plt.gca().set_color_cycle([colormap(i) for i in np.linspace(0,0.9, num_plots)])
+
+file_names = []
+for i in plot_range:
+    file_names.append("../../DD" + `i`.zfill(4) + "/" + "cloud_collision_" + `i`.zfill(4))
+
+labels = []
+for file in file_names:
+
+    ds = yt.load(file)
+    sphere = ds.sphere('max', (75, 'pc'))
+
+    # calculate and stor the bulk velocity of the sphere
+    bulk_velocity = sphere.quantities.bulk_velocity()
+    sphere.set_field_parameter('bulk_velocity', bulk_velocity)
+
+    # create 1d profile object for profiles over radius
+    # and add a velocity profile.
+    prof = yt.create_profile(sphere, 'radius', ('gas', 'velocity_magnitude'),
+        units = {'radius': 'pc'},
+        extrema = {'radius': ((0.1, 'pc'), (75, 'pc'))},
+        weight_field ='cell_mass')
+
+    # create arrays to plot
+    radius = prof.x.value
+    mean = prof['gas', 'velocity_magnitude'].value
+    variance = prof.variance['gas', 'velocity_magnitude'].value
+
+    # plot the variance of the velocity magnitude
+    plt.loglog(radius, variance*1.0E-5, label="Standarad Deviation")
+    labels.append(r"%0.2f Myr" % ds.current_time.value)
+
+plt.xlabel(r"Radius $(\mathrm{pc})$")
+plt.ylabel('v [km/s]')
+plt.xlim(1,75)
+plt.legend(labels, loc="lower left", frameon=False)
+plt.savefig('velocity_disperssion_profiles.png')
